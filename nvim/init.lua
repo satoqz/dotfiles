8e48d0a5f957dcd257a93605891c1a0a8f82dc1a
Jonas Köhnen <mail@satoqz.net>
2024-12-25 06:23:56 +0100

    wip?

---
+++ nvim/init.lua
@@ -0,0 +1,105 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.opt.termguicolors = true
+
+vim.opt.number = true
+vim.opt.signcolumn = "yes"
+
+vim.opt.scrolloff = 8
+
+vim.opt.wrap = false
+vim.opt.linebreak = true
+vim.opt.breakindent = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.hlsearch = true
+vim.opt.smartcase = true
+vim.opt.ignorecase = true
+
+vim.opt.autoindent = true
+
+vim.opt.autoread = true
+vim.opt.undofile = true
+vim.opt.swapfile = false
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.schedule(function()
+  vim.opt.clipboard = "unnamedplus"
+end)
+
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+vim.keymap.set("n", "<Tab>", "<cmd>bnext<CR>")
+vim.keymap.set("n", "<S-Tab>", "<cmd>bprev<CR>")
+vim.keymap.set("n", "<leader>w", "<cmd>bdel!<CR>")
+
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+if not vim.uv.fs_stat(lazypath) then
+  vim.fn.system {
+    "git", "clone", "--filter=blob:none", "--branch=stable",
+    "https://github.com/folke/lazy.nvim.git", lazypath,
+  }
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  "tpope/vim-sleuth",
+  {
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme "gruvbox-material"
+    end,
+  },
+  {
+    "echasnovski/mini.nvim",
+    config = function()
+      require("mini.pairs").setup()
+      require("mini.comment").setup()
+      require("mini.surround").setup()
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = { "j-hui/fidget.nvim" },
+    config = function()
+      -- TODO
+    end
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    main = "nvim-treesitter.configs",
+    opts = { auto_install = true },
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    event = "VimEnter",
+    branch = "0.1.x",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local builtin = require "telescope.builtin"
+      vim.keymap.set("n", "<leader>b", builtin.buffers)
+      vim.keymap.set("n", "<leader>f", builtin.find_files)
+      vim.keymap.set("n", "<leader>/", builtin.live_grep)
+    end,
+  },
+  {
+    "tpope/vim-fugitive",
+    dependencies = { "tpope/vim-rhubarb" },
+  },
+}, {
+  install = {
+    colorscheme = { "gruvbox-material" },
+  },
+})
