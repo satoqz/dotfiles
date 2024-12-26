f93085067a304c644392bfa8c6edba33306838cc
Jonas Köhnen <mail@satoqz.net>
2024-12-26 22:04:35 +0100

    nvim: set fidget winblend to default

--- nvim/lua/satoqz/plugins/lsp.lua
+++ nvim/lua/satoqz/plugins/lsp.lua
@@ -13,10 +13,7 @@ return {
   {
     "neovim/nvim-lspconfig",
     dependencies = {
-      {
-        "j-hui/fidget.nvim",
-        opts = { notification = { window = { winblend = 0 } } },
-      },
+      { "j-hui/fidget.nvim", config = true },
       "saghen/blink.cmp",
     },
     config = function()
