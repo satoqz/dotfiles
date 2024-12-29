321774c9af0972130eb0e6daa7c40490d0af4562
Jonas Köhnen <mail@satoqz.net>
2024-12-29 23:32:24 +0100

    nvim: more keybind experiments

--- nvim/lua/satoqz/keymap.lua
+++ nvim/lua/satoqz/keymap.lua
@@ -1,7 +1,12 @@
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
-vim.keymap.set("n", "gn", "<C-i>")
-vim.keymap.set("n", "gp", "<C-o>")
+vim.keymap.set("i", "<C-h>", "<Left>")
+vim.keymap.set("i", "<C-j>", "<Down>")
+vim.keymap.set("i", "<C-k>", "<Up>")
+vim.keymap.set("i", "<C-l>", "<Right>")
+
+vim.keymap.set("n", "gn", "<cmd>bnext<CR>")
+vim.keymap.set("n", "gp", "<cmd>bprev<CR>")
 
 vim.keymap.set("i", "<C-s>", vim.lsp.buf.signature_help)
 vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
