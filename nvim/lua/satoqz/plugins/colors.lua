32193a8123999637c00e7c3b3a03895325083169
Jonas Köhnen <mail@satoqz.net>
2024-12-29 03:18:34 +0100

    nvim: tree-sitter highlighting never on???

--- nvim/lua/satoqz/plugins/colors.lua
+++ nvim/lua/satoqz/plugins/colors.lua
@@ -3,7 +3,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     main = "nvim-treesitter.configs",
-    opts = { auto_install = true },
+    opts = { auto_install = true, highlight = { enable = true } },
   },
 
   {
