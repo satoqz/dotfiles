83281f044eb8bd7e44c433bc5148a2823191d329
Jonas Köhnen <mail@satoqz.net>
2024-12-30 01:49:24 +0100

    nvim: refactor statusline into several functions

--- nvim/lua/satoqz/init.lua
+++ nvim/lua/satoqz/init.lua
@@ -1,3 +1,4 @@
 require("satoqz.options")
 require("satoqz.keymap")
+require("satoqz.statusline")
 require("satoqz.lazy")
--- nvim/lua/satoqz/options.lua
+++ nvim/lua/satoqz/options.lua
@@ -21,6 +21,7 @@ vim.opt.swapfile = false
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.clipboard = "unnamedplus"
+vim.opt.statusline = "%!v:lua.require('satoqz.statusline').format()"
 vim.opt.autoread = true
 vim.opt.exrc = true
 
@@ -46,58 +47,3 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.wo.colorcolumn = "50,72"
   end,
 })
-
-function StatusLine()
-  local bufnr = vim.fn.winbufnr(vim.g.statusline_winid)
-
-  local path_component = "%f"
-  if vim.bo[bufnr].buftype == "" then
-    local buf_name = vim.api.nvim_buf_get_name(bufnr)
-    if buf_name == "" then
-      path_component = "[No Name]"
-    else
-      path_component = vim.fn.fnamemodify(buf_name, ":~:.")
-    end
-  end
-
-  local modified_component = vim.bo[bufnr].modified and " [+]" or ""
-  local readonly_component = vim.bo[bufnr].readonly and " [readonly]" or ""
-
-  local statusline_type = vim.api.nvim_get_current_win() == vim.g.statusline_winid and "" or "Nc"
-
-  for _, kind in pairs({ "Error", "Warn" }) do
-    vim.api.nvim_set_hl(0, "Statusline" .. kind .. statusline_type, {
-      fg = vim.api.nvim_get_hl(0, { name = "Diagnostic" .. kind }).fg,
-      bg = vim.api.nvim_get_hl(0, { name = "Statusline" .. statusline_type }).bg,
-    })
-  end
-
-  local function colorize(color, component)
-    return "%#" .. color .. "#" .. component .. "%*"
-  end
-
-  local diagnostics = vim.diagnostic.get(bufnr)
-
-  local errors = vim.tbl_filter(function(diagnostic)
-    return diagnostic.severity == vim.diagnostic.severity.ERROR
-  end, diagnostics)
-  local warnings = vim.tbl_filter(function(diagnostic)
-    return diagnostic.severity == vim.diagnostic.severity.WARN
-  end, diagnostics)
-
-  local error_component = #errors > 0
-      and colorize("StatuslineError" .. statusline_type, " ~" .. #errors)
-    or ""
-  local warning_component = #warnings > 0
-      and colorize("StatuslineWarn" .. statusline_type, " ~" .. #warnings)
-    or ""
-  local diagnostics_component = error_component .. warning_component
-
-  return path_component
-    .. modified_component
-    .. readonly_component
-    .. diagnostics_component
-    .. "%=%l:%c "
-end
-
-vim.opt.statusline = "%!v:lua.StatusLine()"
