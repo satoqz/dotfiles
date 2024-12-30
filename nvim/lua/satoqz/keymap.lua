f7d076a31f792335ebcd3bcfb8d231052ccc5ac1
Jonas Köhnen <mail@satoqz.net>
2024-12-30 05:30:00 +0100

    nvim: keybind to toggle common colorcolumns

--- nvim/lua/satoqz/keymap.lua
+++ nvim/lua/satoqz/keymap.lua
@@ -12,3 +12,7 @@ vim.keymap.set("i", "<C-s>", vim.lsp.buf.signature_help)
 vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
 vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename)
 vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action)
+
+vim.keymap.set("n", "<leader>tc", function()
+  vim.opt.colorcolumn = vim.o.colorcolumn == "" and "80,100,120" or ""
+end)
