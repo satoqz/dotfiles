e7eb6b19ddd079d3208ba883ac3aa927f91022a2
Jonas Köhnen <mail@satoqz.net>
2025-01-01 16:52:03 +0100

    nvim: customize kanagawa theme

--- nvim/lua/satoqz/plugins/colors.lua
+++ nvim/lua/satoqz/plugins/colors.lua
@@ -10,14 +10,26 @@ return {
     "rebelot/kanagawa.nvim",
     lazy = false,
     priority = 1000,
-    config = function()
+    opts = {
+      compile = true,
+      theme = "dragon",
+      background = { dark = "dragon", light = "lotus" },
+      colors = { theme = { all = { ui = { bg_gutter = "none" } } } },
+      overrides = function(colors)
+        local ui = colors.theme.ui
+        return {
+          StatusLine = { bg = ui.bg_p1 },
+          StatusLineNC = { bg = ui.bg_p1 },
+          Pmenu = { fg = ui.shade0, bg = ui.bg_p1 },
+          PmenuSel = { fg = "NONE", bg = ui.bg_p2 },
+          PmenuSbar = { bg = ui.bg_m1 },
+          PmenuThumb = { bg = ui.bg_p2 },
+        }
+      end,
+    },
+    config = function(_, opts)
       local kanagawa = require("kanagawa")
-      kanagawa.setup({
-        compile = true,
-        theme = "dragon",
-        background = { dark = "dragon", light = "lotus" },
-        colors = { theme = { all = { ui = { bg_gutter = "none" } } } },
-      })
+      kanagawa.setup(opts)
       kanagawa.load()
     end,
   },
--- nvim/lua/satoqz/plugins/lsp.lua
+++ nvim/lua/satoqz/plugins/lsp.lua
@@ -51,7 +51,6 @@ return {
     version = "*",
     opts = {
       keymap = { preset = "super-tab" },
-      appearance = { use_nvim_cmp_as_default = true },
       signature = { enabled = true },
       completion = {
         documentation = { auto_show = true },
