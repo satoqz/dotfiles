417529256f8df67dede5a671342289b8b3000c65
Jonas Köhnen <mail@satoqz.net>
2025-01-04 19:51:41 +0100

    fish: set max git prompt branch length

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,6 +1,8 @@
+set -g __fish_git_prompt_showcolorhints 1
+set -g __fish_git_prompt_color_branch red
+set -g __fish_git_prompt_shorten_branch_len 16
+
 function fish_prompt
-    set -g __fish_git_prompt_showcolorhints 1
-    set -g __fish_git_prompt_color_branch red
     printf "%s%s%s%s ➜ " \
         (set_color $fish_color_cwd) (fish_prompt_pwd_full_dirs=2 prompt_pwd) \
         (set_color normal) (fish_git_prompt)
