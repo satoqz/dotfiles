4a0f133b7b1155dbcb4a2a9bf4675451a20bfbf1
Jonas Köhnen <mail@satoqz.net>
2025-01-09 22:14:26 +0100

    fish: tweak colors

--- fish/config.fish
+++ fish/config.fish
@@ -12,8 +12,14 @@ end
 
 if status is-interactive
     set -U fish_greeting
+
+    set fish_color_command green
+    set fish_color_param white
+    set fish_color_comment brblack
+
     fish_vi_key_bindings
     bind --mode insert \cr history-pager
+
     command -v eza >/dev/null; and alias ls="eza -F -A"
     command -v kubectl >/dev/null; and alias k=kubectl
     command -v kubecolor >/dev/null; and alias k=kubecolor
--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,12 +1,7 @@
 function fish_prompt
-    test -n "$SSH_TTY"; and printf "%s$USER@$hostname%s " \
-        (set_color cyan) (set_color normal)
-
-    printf "%s%s%s%s" \
-        (set_color $fish_color_cwd) (fish_prompt_pwd_dir_length=0 prompt_pwd) \
-        (set_color normal)
-
-    printf "\n➜ "
+    test -n "$SSH_TTY"; and printf "$USER@$hostname "
+    printf "%s%s%s%s" (set_color cyan) (fish_prompt_pwd_dir_length=0 prompt_pwd) (set_color normal)
+    printf "\n%s➜%s " (set_color green) (set_color normal)
 end
 
 function fish_prompt_newline --on-event fish_postexec
