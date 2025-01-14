cba114daf9b9f98a013fd392ba8bfc7d367178de
Jonas Köhnen <mail@satoqz.net>
2025-01-14 21:36:53 +0100

    woo

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,6 +1,9 @@
-set -g __fish_git_prompt_showcolorhints 1
-set -g __fish_git_prompt_color_branch red
+set -g __fish_git_prompt_char_stateseparator ""
+set -g __fish_git_prompt_color_branch brred
 set -g __fish_git_prompt_shorten_branch_len 16
+set -g __fish_git_prompt_showcolorhints 1
+set -g __fish_git_prompt_showdirtystate 1
+set -g __fish_git_prompt_showupstream informative
 
 function fish_prompt
     echo -en "\e[6 q" # Resets cursor to line cursor.
@@ -8,7 +11,7 @@ function fish_prompt
     test -n "$SSH_TTY"; and printf "%s$USER@$hostname%s " \
         (set_color cyan) (set_color normal)
 
-    printf "%s%s%s%s > " \
-        (set_color $fish_color_cwd) (fish_prompt_pwd_dir_length=0 prompt_pwd) \
+    printf "%s%s%s%s >> " \
+        (set_color brgreen) (string split -- / (prompt_pwd))[-1] \
         (set_color normal) (fish_git_prompt)
 end
