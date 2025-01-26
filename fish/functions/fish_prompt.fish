beb2116cb84bd48991e0d3ea231c2cb95955a069
Jonas Köhnen <mail@satoqz.net>
2025-01-26 04:52:37 +0100

    fix(fish): fix empty path in prompt if at root dir

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -6,12 +6,12 @@ set -g __fish_git_prompt_showdirtystate 1
 set -g __fish_git_prompt_showupstream informative
 
 function fish_prompt
-    echo -en "\e[6 q" # Resets cursor to line cursor.
+    echo -en "\e[6 q" # Reset to bar cursor.
 
     test -n "$SSH_TTY" && printf "%s$USER@$hostname%s " \
         (set_color cyan) (set_color normal)
 
     printf "%s%s%s%s >> " \
-        (set_color brgreen) (string split -- / (prompt_pwd))[-1] \
+        (set_color brgreen) (path basename (prompt_pwd)) \
         (set_color normal) (fish_git_prompt)
 end
