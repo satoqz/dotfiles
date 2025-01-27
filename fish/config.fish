22921bbae0246dccf2f18a2060da6e1126dd453c
Jonas Köhnen <mail@satoqz.net>
2025-01-27 15:21:46 +0100

    ok we go zed...

--- fish/config.fish
+++ fish/config.fish
@@ -6,11 +6,9 @@ fish_add_path -m $GOPATH/bin
 fish_add_path -m ~/.cargo/bin
 fish_add_path -m ~/.local/bin
 
-command -q hx && not set -q EDITOR && set -gx EDITOR hx
 command -q eza && alias ls="eza -F -A"
 command -q kubectl && alias k=kubectl
 command -q kubecolor && alias k=kubecolor
 command -q direnv && direnv hook fish | source
-command -q fd && command -q fzy && bind \cg goto_repo
 
 set -U fish_greeting
--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -6,12 +6,7 @@ set -g __fish_git_prompt_showdirtystate 1
 set -g __fish_git_prompt_showupstream informative
 
 function fish_prompt
-    echo -en "\e[6 q" # Reset to bar cursor.
-
-    test -n "$SSH_TTY" && printf "%s$USER@$hostname%s " \
-        (set_color cyan) (set_color normal)
-
-    printf "%s%s%s%s >> " \
+    printf "%s%s%s%s > " \
         (set_color brgreen) (path basename (prompt_pwd)) \
         (set_color normal) (fish_git_prompt)
 end
