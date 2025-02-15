de0422ade526b03bcf26ef7c0a2b15469f4822bb
Jonas Köhnen <mail@satoqz.net>
2025-02-15 17:42:00 +0100

    menno

--- fish/conf.d/aliases.fish
+++ fish/conf.d/aliases.fish
@@ -1,4 +1,4 @@
-command -q eza && alias ls="eza -A -F --icons --group-directories-first"
+command -q eza && alias ls="eza -A -F --group-directories-first"
 command -q kubectl && alias k=kubectl
 command -q kubecolor && alias k=kubecolor
 command -q nvim && alias vim=nvim && alias vi=nvim
