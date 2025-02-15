04141b76fc64cd6b3bb626695804f27aa61cb5d9
Jonas Köhnen <mail@satoqz.net>
2025-02-15 16:17:36 +0100

    iosevka nerd font mono for better icons

--- fish/conf.d/aliases.fish
+++ fish/conf.d/aliases.fish
@@ -1,3 +1,3 @@
-command -q eza && alias ls="eza -A -F --group-directories-first"
+command -q eza && alias ls="eza -A -F --icons --group-directories-first"
 command -q kubectl && alias k=kubectl
 command -q kubecolor && alias k=kubecolor
