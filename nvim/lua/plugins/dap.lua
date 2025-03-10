ec57a65ab6661d51eb845b5602cc58c237698ed6
Jonas Köhnen <mail@satoqz.net>
2025-03-10 16:55:56 +0100

    nvim: tweak dap setup

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -1,37 +1,30 @@
 return {
   {
     "mfussenegger/nvim-dap",
-    dependencies = { "rcarriga/nvim-dap-ui", "leoluz/nvim-dap-go" },
+    dependencies = { "leoluz/nvim-dap-go" },
     cmd = { "DapNew", "DapContinue", "DapToggleBreakpoint" },
     keys = {
-      { "<leader>db", function() require("dap").toggle_breakpoint() end },
       { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input("Condition: ")) end },
+      { "<leader>db", function() require("dap").toggle_breakpoint() end },
       { "<leader>dc", function() require("dap").continue() end },
-      { "<leader>dp", function() require("dap").pause() end },
-      { "<leader>dt", function() require("dap").terminate() end },
-      { "<leader>dl", function() require("dap").run_last() end },
+      { "<leader>df", function() require("dap").focus_frame() end },
       { "<leader>dh", function() require("dap").run_to_cursor() end },
       { "<leader>di", function() require("dap").step_into() end },
-      { "<leader>do", function() require("dap").step_out() end },
-      { "<leader>dn", function() require("dap").step_over() end },
       { "<leader>dj", function() require("dap").down() end },
       { "<leader>dk", function() require("dap").up() end },
+      { "<leader>dl", function() require("dap").list_breakpoints(true) end },
+      { "<leader>dn", function() require("dap").step_over() end },
+      { "<leader>do", function() require("dap").step_out() end },
+      { "<leader>dp", function() require("dap").pause() end },
+      { "<leader>dr", function() require("dap").repl.toggle() end },
+      { "<leader>dt", function() require("dap").terminate() end },
       { "<leader>dw", function() require("dap.ui.widgets").hover() end },
     },
-  },
-  {
-    "rcarriga/nvim-dap-ui",
-    dependencies = { "nvim-neotest/nvim-nio" },
     config = function()
-      local dap, dapui = require("dap"), require("dapui")
-      dapui.setup()
-
-      dap.listeners.before.attach.dapui_config = dapui.open
-      dap.listeners.before.launch.dapui_config = dapui.open
+      vim.fn.sign_define("DapStopped", { text = "→", texthl = "green", linehl = "Visual" })
+      vim.fn.sign_define("DapBreakpoint", { text = "•", texthl = "red" })
+      vim.fn.sign_define("DapBreakpointCondition", { text = "?", texthl = "red" })
+      vim.fn.sign_define("DapBreakpointRejected", { text = "!", texthl = "red" })
     end,
-    keys = {
-      { "<leader>du", function() require("dapui").toggle() end },
-      { "<leader>de", function() require("dapui").eval() end, mode = { "n", "v" } },
-    },
   },
 }
