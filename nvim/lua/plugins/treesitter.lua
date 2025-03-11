6794f7d7bd386be732d2a1e454ac4b68ea2c9bf1
Jonas Köhnen <mail@satoqz.net>
2025-03-11 21:40:40 +0100

    treesitter context, take two

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -15,19 +15,19 @@ return {
           enable = true,
           set_jumps = true,
           goto_next_start = {
-            ["]c"] = "@class.outer",
+            ["]s"] = "@class.outer",
             ["]f"] = "@function.outer",
           },
           goto_previous_start = {
-            ["[c"] = "@class.outer",
+            ["[s"] = "@class.outer",
             ["[f"] = "@function.outer",
           },
           goto_next_end = {
-            ["]C"] = "@class.outer",
+            ["]S"] = "@class.outer",
             ["]F"] = "@function.outer",
           },
           goto_previous_end = {
-            ["[C"] = "@class.outer",
+            ["[S"] = "@class.outer",
             ["[F"] = "@function.outer",
           },
         },
@@ -36,8 +36,8 @@ return {
           lookahead = true,
           include_surounding_whitespace = true,
           keymaps = {
-            ["ac"] = "@class.outer",
-            ["ic"] = "@class.inner",
+            ["as"] = "@class.outer",
+            ["is"] = "@class.inner",
             ["af"] = "@function.outer",
             ["if"] = "@function.inner",
           },
@@ -48,6 +48,23 @@ return {
   {
     "nvim-treesitter/nvim-treesitter-textobjects",
     event = "VeryLazy",
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    event = "VeryLazy",
+    opts = { multiwindow = true },
+    keys = {
+      { "[c", function() require("treesitter-context").go_to_context(vim.v.count1) end },
+    },
+    init = function()
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        group = vim.api.nvim_create_augroup("satoqz-treesitter-context-highlights", { clear = true }),
+        callback = function()
+          vim.api.nvim_set_hl(0, "TreesitterContext", { link = "Normal" })
+          vim.api.nvim_set_hl(0, "TreesitterContextBottom", { underline = true, sp = "Grey" })
+        end,
+      })
+    end,
   },
   {
     "VidocqH/auto-indent.nvim",
